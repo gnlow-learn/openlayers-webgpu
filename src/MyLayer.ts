c123901e55afed6ec4ab137175be446ca06cc01e
Gnlow <gnlow@pm.me>
2025-01-29 19:34:12 +0900

    it works

--- src/MyLayer.ts
+++ src/MyLayer.ts
@@ -1,13 +1,16 @@
 import { Layer, FrameState } from "./deps.ts"
 // @ts-types="https://denopkg.com/gnlow/lilgpu@0535935/browser.ts"
-import { initCanvas } from "https://esm.sh/gh/gnlow/lilgpu@0535935/browser.ts"
+import { initCanvas, d } from "https://esm.sh/gh/gnlow/lilgpu@0535935/browser.ts"
+import { lefebvre } from "./ColorMap.ts"
+
+const shader = await fetch("src/main.wgsl").then(X => X.text())
 
 export class MyLayer extends Layer {
     canvas
     draw
     constructor(
         canvas: HTMLCanvasElement,
-        draw: () => HTMLCanvasElement,
+        draw: (extent: number[]) => HTMLCanvasElement,
     ) {
         super({})
         this.canvas = canvas
@@ -19,31 +22,45 @@ export class MyLayer extends Layer {
         console.log(frameState.extent, frameState.viewState.projection.getCode())
         this.canvas.width = frameState.size[0]
         this.canvas.height = frameState.size[1]
-        return this.draw()
+        return this.draw(frameState.extent!)
     }
 
     static async from() {
         const canvas = document.createElement("canvas")
         const g = await initCanvas({
-            vertShader: `
-                @vertex
-                fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-                    let x = f32(i32(in_vertex_index) - 1);
-                    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-                    return vec4<f32>(x, y, 0.0, 1.0);
-                }
-            `,
-            fragShader: `
-                @fragment
-                fn fs_main() -> @location(0) vec4<f32> {
-                    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
-                }
-            `,
+            vertShader: shader,
+            fragShader: shader,
             canvas,
+            layout: {
+                colorMap: { uniform:
+                    d.arrayOf(d.vec3u, 256)
+                },
+                extent: { uniform:
+                    d.struct({
+                        minX: d.f32,
+                        minY: d.f32,
+                        maxX: d.f32,
+                        maxY: d.f32,
+                    })
+                },
+            },
         })
-        return new MyLayer(canvas, () => {
-            g.draw(3)
-            return canvas
-        })
+
+        g.buffers.colorMap.write(
+            Array.from({ length: 256 }, (_, i) =>
+                d.vec3u(...lefebvre.get(i))
+            )
+        )
+
+        return new MyLayer(
+            canvas,
+            ([minX, minY, maxX, maxY]) => {
+                g.buffers.extent.write({
+                    minX, minY, maxX, maxY,
+                })
+                g.draw(4)
+                return canvas
+            }
+        )
     }
 }
